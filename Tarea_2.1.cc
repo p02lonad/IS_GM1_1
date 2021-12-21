5b36ac96f1c90e7852baeb3b6b7269eff3b8109c
mcili0 <91116326+mcili0@users.noreply.github.com>
2021-12-22 00:03:31 +0100

    Add files via upload

---
+++ Tarea_2.1.cc
@@ -0,0 +1,11 @@
+int main(){
+  int i, name, apellido1, apellido2;
+  cout<<"Introduzca el numero de usuarios que quiere introducir al sistema<<"endl;
+  cin>>;
+  for(int n=i; n>0; n--){
+    cout<<"Introduzca nombre"<<endl;
+    cin>>name;
+    cout<<"Introduzca los apellidos"<<endl;
+    cin>>apellido1;
+    cin>>apellido2;}
+    }
