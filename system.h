6aa1e561b64184c8db8a3b956b1270b70b5db965
mcili0 <91116326+mcili0@users.noreply.github.com>
2021-12-21 23:56:15 +0100

    Update system.h

--- system.h
+++ system.h
@@ -7,19 +7,34 @@
     class Usuario
     {
         private:
+            string Nombreusuario_;
+            string Contrasena;
         public:
+            void getLista();
+            void CambiarContrasena(string nombreUsuario);
     };
     
     class Reserva
     {
         private:
+            string Fecha_;
+            int Duracion;
         public:
+        void ModificarReserva(string fecha)
+        void CancelarReserva(string fecha)
+        void Reservar(string fecha);
     };
     
     class Maquina
     {
         private:
+            int Nucleos_
+            bool Disponibilidad_
+            int Idmaquina_
         public:
+            bool comprobarDisponibilidad(string fecha, string nucleos)
+            void modificarMaquinas(string fecha, string nucleos)
+            void eliminarMaquinas(string fecha, string nucleos)
     };
 
 
