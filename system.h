2bf5897f1d05bf771a353592d959bcbda3ce8f02
mcili0 <91116326+mcili0@users.noreply.github.com>
2021-12-21 23:42:40 +0100

    Add files via upload

---
+++ system.h
@@ -0,0 +1,26 @@
+#ifndef system_h
+#define system_h
+
+    #include <iostream>
+    #include <cstdlib>
+
+    class Usuario
+    {
+        private:
+        public:
+    };
+    
+    class Reserva
+    {
+        private:
+        public:
+    };
+    
+    class Maquina
+    {
+        private:
+        public:
+    };
+
+
+#endif
